4517a9663c00375d366dee299260be0939269d95
David Morrow <quickbasic44@outlook.com>
2023-05-04 13:59:57 -0400

    To Transform and scale version

--- MauiGraph/HeartPage.xaml.cs
+++ MauiGraph/HeartPage.xaml.cs
@@ -1,16 +1,20 @@
+using Microsoft.Maui.Controls.Shapes;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace MauiGraph;
 
 public partial class HeartPage : ContentPage
 {
-	public HeartPage()
+    public HeartPage()
 	{
 		InitializeComponent();
 		Label1.Text = this.Width.ToString() + " " + this.Height.ToString();
-	}
+		BindingContext = this;
+    }
 
     private void PointerGestureRecognizer_PointerMoved(object sender, PointerEventArgs e)
     {
-		Label1.Text = e.GetPosition(MyGrid).Value.ToString();
-
+		Label1.Text = e.GetPosition(MyLayout).Value.ToString();
     }
 }
